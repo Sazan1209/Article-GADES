ed991e141c6f06c1c7452f58db76b0a8178e1a58
Sazan1209 <Sazanovich.md@phystech.edu>
2025-05-13 13:28:44 +0300

    Fixes for runners

--- scripts/Benchmarking/runners/cpu_py_sparse_generated_run_benchmark.sh
+++ scripts/Benchmarking/runners/cpu_py_sparse_generated_run_benchmark.sh
@@ -13,8 +13,9 @@ do
     for features in "10" "100" "1000" "10000" "100000"
     do
       for sparsity in "0.5" "0.75" "0.9" "0.95" "0.99"
+      do
         num_elements=$(( cells * features ))
-        if [[ $num_elements > 10000000 || $num_elements < 100000]]
+        if [[ $num_elements > 10000000 || $num_elements < 100000 ]]
         then
           continue
         fi
--- scripts/Benchmarking/runners/cpu_r_sparse_generated_run_benchmark.sh
+++ scripts/Benchmarking/runners/cpu_r_sparse_generated_run_benchmark.sh
@@ -13,23 +13,27 @@ do
   do
     for features in "10" "100" "1000" "10000" "100000"
     do
-      num_elements=$(( cells * features ))
-
-      if [[ $num_elements > 10000000 || $num_elements < 100000]]
-      then
-        continue
-      fi
-      input="${ROOT_FOLDER}"/Datasets/Generated/${cells}_cells_${features}_features.csv
-      folder="${ROOT_FOLDER}"/results/GeneratedDense/${cells}_cells_${features}_features/
-      mkdir -p "$folder"
-      for metric in "spearman" "cosine" "manhattan" #"kendall"
+      for sparsity in "0.5" "0.75" "0.9" "0.95" "0.99"
       do
-        if [[ $metric == "cosine" && $method == "factoextra" ]]; then continue; fi
-        name="benchmark_"${method}_${metric}_${cells}x${features}
-        output="$folder"/_${method}_${metric}.csv
-        if [[ $metric == "manhattan" ]]; then output="$folder"/_${method}_l1.csv; fi
-        logs="${ROOT_FOLDER}"/logs/$name
-        sbatch --job-name=$name  --cpus-per-task=24 -o "$logs" -D $(dirname $script) "$script" "$input" $method 25 $metric "$output" TRUE FALSE 24
+        num_elements=$(( cells * features ))
+
+        if [[ $num_elements > 10000000 || $num_elements < 100000 ]]
+        then
+          continue
+        fi
+        input="${ROOT_FOLDER}"/Datasets/GeneratedSparse/${cells}_cells_${features}_features/$sparsity.mtx
+        folder="${ROOT_FOLDER}"/results/GeneratedSparse/${cells}_cells_${features}_features
+        mkdir -p "$folder"
+        for metric in "spearman" "cosine" "manhattan" #"kendall"
+        do
+          if [[ $metric == "cosine" && $method == "factoextra" ]]; then continue; fi
+          name=${method}_${metric}_${cells}x${features}x${sparsity}"_benchmark"
+          output="$folder"/${sparsity}_${method}_${metric}.csv
+          logs="${ROOT_FOLDER}/logs/$name.out"
+          if [[ $metric == "manhattan" ]]; then output="$folder"/${sparsity}_${method}_l1.csv; fi
+          logs="${ROOT_FOLDER}"/logs/$name
+          sbatch --job-name=$name  --cpus-per-task=24 -o "$logs" -D $(dirname $script) "$script" "$input" $method 25 $metric "$output" TRUE FALSE 24
+        done
       done
     done
   done
