347e3dd0fbd4757a9ff44ff496381761e2fd66e3
Sazan1209 <Sazanovich.md@phystech.edu>
2025-05-14 14:10:46 +0300

    Fixed R benchmarking transpose issue (probably)

--- scripts/Benchmarking/R_benchmarking.R
+++ scripts/Benchmarking/R_benchmarking.R
@@ -17,14 +17,12 @@ if (profile) {
     library(profmem)
     library(ps)
 }
-print(glue('Reading table. Output folder will be {output}_{method}_{metric}.csv'))
 
 if (sparse) {
     data <- readMM(datain)
 } else {
-    data <- t(as.matrix(read.table(datain, header=T, row.names = 1, sep=",")))
+    data <- as.matrix(read.table(datain, header=T, row.names = 1, sep=","))
 }
-print('Completed reading')
 
 
 
@@ -35,45 +33,23 @@ for (i in 1:times) {
 
     if (method == 'amap') {
         if (metric == 'cosine'){
-          distMatrix_mtrx <- as.matrix(Dist(t(data), method='pearson', nbproc=threads))
+          distMatrix_mtrx <- as.matrix(Dist(data, method='pearson', nbproc=threads))
         } else {
-          distMatrix_mtrx <- as.matrix(Dist(t(data), method=metric, nbproc=threads))
+          distMatrix_mtrx <- as.matrix(Dist(data, method=metric, nbproc=threads))
         }
     } else if (method == 'factoextra') {
-        if (!sparse) {
+        if (sparse) {
             data <- as.matrix(data)
         }
 
-        distMatrix_mtrx <- as.matrix(get_dist(t(data), method = metric))
+        distMatrix_mtrx <- as.matrix(get_dist(data, method = metric))
     }
+
     end_time <- as.numeric(Sys.time()) * 1000000
     measurements[i] <- end_time - st_t
-
-    if (profile) {
-        p <- profmem_end()
-        if (method == 'amap' || method == 'factoextra') {
-            delta <- object.size(distMatrix_mtrx)
-        } else {
-            delta <- 0
-        }
-        print(p)
-        sum_bytes <- sum(p$bytes, na.rm=T)
-        end <- as.numeric(ps::ps_memory_info()['rss'][1])
-        print('Memory usage')
-        print(c(sum_bytes, delta))
-
-        delta_manual <- 0
-        print(method)
-        memories[i, 1] <- sum_bytes + delta_manual
-        memories[i, 2] <- delta
-        memories[i, 3] <- delta_manual
-        memories[i, 4] <- end - start
-    }
-    
+    print(distMatrix_mtrx.nrow, distMatrix_mtrx.ncol)
     gc()
 }
 
-if (profile){
-    write.table(memories, glue("{output}_{method}_{metric}_memory.csv", sep=','))
-}
 write.table(measurements, output, sep=',')
+
